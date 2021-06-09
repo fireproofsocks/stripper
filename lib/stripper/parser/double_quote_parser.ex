59385db6aae23a35b5c40c21a09fe478a336464e
Everett Griffiths <everett@fireproofsocks.com>
2021-06-09 14:37:47 -0400

    Use ? notation to better demonstrate the substitution to a double quote

--- lib/stripper/parser/double_quote_parser.ex
+++ lib/stripper/parser/double_quote_parser.ex
@@ -24,12 +24,11 @@ defmodule Stripper.Parser.DoubleQuoteParser do
 
       # Some ad-hoc single quote characters not defined by the guard
       defp parse(<<head::utf8, rest::binary>>, acc, meta) when head in ~c[《》「」] do
-        parse(rest, acc <> <<34::utf8>>, meta)
+        parse(rest, acc <> <<?">>, meta)
       end
 
       defp parse(<<head::utf8, rest::binary>>, acc, meta) when is_quote_mark_double(head) do
-        # code point 34 is your regular straight double-quote
-        parse(rest, acc <> <<34::utf8>>, meta)
+        parse(rest, acc <> <<?">>, meta)
       end
     end
   end

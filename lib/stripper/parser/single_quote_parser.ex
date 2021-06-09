796b4afae7c4a3f3be10ab8359c9ef82526fcf38
Everett Griffiths <everett@fireproofsocks.com>
2021-06-09 14:21:02 -0400

    Simplifies guard clause statements

--- lib/stripper/parser/single_quote_parser.ex
+++ lib/stripper/parser/single_quote_parser.ex
@@ -11,14 +11,13 @@ defmodule Stripper.Parser.SingleQuoteParser do
       defguard is_quote_mark_single(char)
                when is_integer(char) and
                       (char == 39 or
-                         ((is_integer(char) and (char >= 8216 and char <= 8219)) or
-                            ((is_integer(char) and (char >= 8249 and char <= 8250)) or
-                               ((is_integer(char) and (char >= 10075 and char <= 10076)) or
-                                  ((is_integer(char) and (char >= 12300 and char <= 12303)) or
-                                     ((is_integer(char) and (char >= 65089 and char <= 65092)) or
-                                        (char == 65287 or
-                                           ((is_integer(char) and
-                                               (char >= 65378 and char <= 65379)) or false))))))))
+                         (char >= 8216 and char <= 8219) or
+                         (char >= 8249 and char <= 8250) or
+                         (char >= 10075 and char <= 10076) or
+                         (char >= 12300 and char <= 12303) or
+                         (char >= 65089 and char <= 65092) or
+                         char == 65287 or
+                         (char >= 65378 and char <= 65379))
 
       # Some ad-hoc single quote characters not defined by the guard
       defp parse(<<head::utf8, rest::binary>>, acc, meta) when head in ~c[`〈〉『』] do
